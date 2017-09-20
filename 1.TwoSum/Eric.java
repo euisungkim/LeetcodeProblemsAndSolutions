3ab9b887a9c5ca058f90e2e4c8b1b9d697715763
Eric Kwon <ekwon@Erics-MacBook-Pro.local>
2017-09-20 11:26:07 -0400

    Eric: Added Two Sum Solution

---
+++ 1.TwoSum/Eric.java
@@ -0,0 +1,35 @@
+
+// Name: Eric Kwon
+// Date: September 18th, 2017
+// From: LeetCode
+// Problem:
+// Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        
+        // Hash map to store the array values as the KEYS
+        // Store the array indices as the VALUES
+        HashMap<Integer, Integer> list = new HashMap<>();
+        int arraySize = nums.length;
+        for (int i=0 ; i < arraySize ; i++) {
+            list.put (nums[i], i);
+        }
+        
+        // Create the array that will store the resulting indices
+        int[] result = new int[2];
+        
+        // Scan through the hash map after creating an integer value that stores TARGET - NUMS ARRAY @ i
+        // Use containsKey() to find the mapped element and return the value stored for the KEY
+        // && operator added to ensure that same element is not used twice
+        for (int i=0 ; i <arraySize ; i++) {
+            int difference = target - nums[i];
+            if (list.containsKey(difference) && list.get(difference) != i) {
+                result[0] = i;
+                result[1] = list.get(difference);
+            }
+        } 
+        return result;
+    }
+}
