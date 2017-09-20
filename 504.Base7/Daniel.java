f416a2312bc3a40c1787c289885bdcd3879add57
Dan ES Kim <euisungkim@hotmail.com>
2017-09-20 12:39:58 -0400

    Daniel: Added Solution to Base7

---
+++ 504.Base7/Daniel.java
@@ -0,0 +1,41 @@
+/*
+Name: Eui Sung Kim
+Date: September 20 2017
+
+Problem:
+Given an integer, return its base 7 string representation.
+
+Example 1:
+Input: 100
+Output: "202"
+*/
+
+class Daniel
+{
+    public String convertToBase7(int num)
+    {
+        StringBuilder sb = new StringBuilder();
+        if ( num < 7 && num > -7 )
+        {
+            return sb.append(num).toString();
+        }
+        
+        boolean numIsNeg = num < 0;
+        if ( numIsNeg )
+            num = Math.abs(num);
+        
+        int ans = 0, tenth = 1;
+        while ( num > 6 )
+        {
+            ans += (num % 7) * tenth;
+            tenth *= 10;
+            num /= 7;
+        }
+        ans += num * tenth;
+        
+        if ( numIsNeg )
+            ans *= -1;
+        
+        return sb.append(ans).toString();
+    }
+}
