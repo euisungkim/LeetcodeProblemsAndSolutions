b25a727974ec244b3de17c5db5a1ecf3e551c060
Eric Kwon <eric.kwon35@qmail.cuny.edu>
2017-09-20 20:57:51 -0400

    Added solution to problem #643

---
+++ 643.MaxSubarrayAvg1/Eric.java
@@ -0,0 +1,38 @@
+
+// Name: Eric Kwon
+// Date: September 20th, 2017
+// From: LeetCode
+// Problem:
+// Given an array consisting of n integers, find the contiguous subarray of given length k that has the maximum average value. 
+// And you need to output the maximum average value.
+
+
+class Solution {
+    public double findMaxAverage(int[] nums, int k) {
+        int arraySize = nums.length;
+
+        // Placeholder for maximum average, and instantaneous average
+        double max_average = 0;
+        double inst_average = 0;
+        
+        // First maximum array from index 0 to k
+        for (int i=0 ; i < k ; i++) {
+            max_average += nums[i];
+        }
+        
+        // Duplicate to allow application of sliding window
+        inst_average = max_average;
+        
+        // Per each iteration value at 'i-1' index is subtracted and value at 'i+k-1' is added
+        // 'i-1' index immediately left to the target array
+        // 'i+k-1' index immediately right to the target array
+        for (int i=1 ; i <= (arraySize - k) ; i++) {
+            inst_average = inst_average - nums[i-1];
+            inst_average = inst_average + nums[i+k-1];
+            max_average = Math.max(max_average, inst_average);
+        }
+        
+        return max_average / k;
+        
+    }
+}
--- README.md
+++ README.md
@@ -1,14 +1,15 @@
 # Leetcode Problems and Solutions
 
-Purpose of this repository is to motivate ourselves to solve leetcode problems daily.
+Purpose of this repository is to motivate ourselves to solve LeetCode problems daily.
 
 
 ## Easy Problems
 
-|Problem|Daniel| Date | Eric | Date | Arafat | Date |
-|-------|:----:|:----:|:----:|:----:|:------:|:----:|
-|[Two Sum](https://leetcode.com/problems/two-sum/description/)|[Sol#1](./1.TwoSum/Daniel.java)| 09/20/17 |[Sol#1](./1.TwoSum/Eric.java)| 09/20/17 |  [Sol] |  |
-|[Base 7](https://leetcode.com/problems/base-7/description/)|[Sol#1](./504.Base7/Daniel.java)| 09/20/17 |[Sol#1](./504.Base7/Eric.java)| 09/20/17 | [Sol] | |
+|Problem| - |Daniel| Date | - | Eric | Date | - | Arafat | Date |
+|-------|:-:|:----:|:----:|:-:|:----:|:----:|:-:|:------:|:----:|
+|[Two Sum](https://leetcode.com/problems/two-sum/description/)||[Sol#1](./1.TwoSum/Daniel.java)|09/20/17||[Sol#1](./1.TwoSum/Eric.java)|09/20/17||[Sol]||
+|[Base 7](https://leetcode.com/problems/base-7/description/)||[Sol#1](./504.Base7/Daniel.java)|09/20/17||[Sol#1](./504.Base7/Eric.java)|09/20/17||[Sol]||
+|[Maximum Subarray Average #1](https://leetcode.com/problems/maximum-average-subarray-i/description/)||[Sol]|||[Sol#1](./643.MaxSubarrayAvg1/Eric.java)|09/20/17||[Sol]||
 
 ## Medium Problems
 
